bdf9d58b3484510529403a7f59710214e197b7e9
Sahaj Rastogi <sahaj.rastogi@gmail.com>
2023-10-29 06:22:07 -0700

    cors

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 //require('dotenv').config()
 import {} from 'dotenv/config'
 import express from 'express';
+import cors from 'cors';
 const app = express()
 app.use(express.json())
+app.use(cors())
 import axios from 'axios';
 // const axios = require('axios');
 
